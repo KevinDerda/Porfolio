2e7d47d5ea85438557fb65f6eb3a6500f5f0d7ae
NitramJulien <dev@enyosolutions.com>
2021-02-18 15:00:55 +0100

    Add script for cookie banner

---
+++ myScript.js
@@ -0,0 +1,23 @@
+(function () {
+  const acceptCookie = localStorage.getItem('accept-cookie');
+  if (!acceptCookie) {
+    displayCookieBanner();
+  }
+})();
+
+/**
+ * AFFICHER BANIERE
+ */
+function displayCookieBanner() {
+  const cookieBanner = document.getElementById('cookie-banner')
+  cookieBanner.classList.toggle('show')
+}
+
+/**
+ * CACHER BANNIERE
+ */
+function hideCookieBanner() {
+  localStorage.setItem('accept-cookie', true);
+  const cookieBanner = document.getElementById('cookie-banner')
+  cookieBanner.classList.toggle('show')
+}
